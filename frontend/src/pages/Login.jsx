012ea3f8f4e1a8216287de6c50a82423083ad6f9
Ahmed Hamdy <ahmed.hamdy@wyz-solutions.com>
2022-05-20 07:47:05 +0200

    Added register & login code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,64 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
 function Login() {
-	return <div>Login</div>;
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const { email, password } = formData;
+
+	const getValue = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaSignInAlt />
+					Login
+				</h1>
+				<p>Please login</p>
+			</section>
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="email"
+							placeholder="Email"
+							id="email"
+							name="email"
+							value={email}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="password"
+							placeholder="Password"
+							id="password"
+							name="password"
+							value={password}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<button className="btn btn-block" type="submit">
+							Submit
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,88 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Register() {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		password2: "",
+	});
+
+	const { name, email, password, password2 } = formData;
+
+	const getValue = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaUser />
+					Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="text"
+							placeholder="Name"
+							id="name"
+							name="name"
+							value={name}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="email"
+							placeholder="Email"
+							id="email"
+							name="email"
+							value={email}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="password"
+							placeholder="Password"
+							id="password"
+							name="password"
+							value={password}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="password"
+							placeholder="Confirm Password"
+							id="password2"
+							name="password2"
+							value={password2}
+							onChange={getValue}
+						/>
+					</div>
+					<div className="form-group">
+						<button className="btn btn-block" type="submit">
+							Submit
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Register;
